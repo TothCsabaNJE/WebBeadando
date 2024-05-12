a0360039060f83444d2b353f4c94a6bf3e602f89
Tóth Csaba <lagolas420@gmail.com>
2024-05-12 18:38:03 +0200

    Update mainpage.php

--- WebProject/mainpage.php
+++ WebProject/mainpage.php
@@ -3,6 +3,7 @@
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>WebNev</title>
     <?php
         $host = 'localhost';
@@ -11,24 +12,28 @@
         $password = '';
         ?>
 </head>
-<nav>
-    <div class="hornav">
-    <ul>
-        <li><a href="mainpage.php?page=home">Home</a></li>
-        <li><a href="mainpage.php?page=account">Account</a></li>
-        <li><a href="mainpage.php?page=chats">Messages</a></li>
-        <li><a href="mainpage.php?page=feedback">Feedback</a></li>
-        <li><a href="mainpage.php?page=gallery">Gallery</a></li>
-        <li><a href="./">Logout</a></li>
-    </ul>   
+<div class="container">
+    <div class="col-xl-12">
+        <nav>
+            <ul>
+                <li><a href="mainpage.php?page=home">Home</a></li>
+                <li><a href="mainpage.php?page=account">Account</a></li>
+                <li><a href="mainpage.php?page=chats">Messages</a></li>
+                <li><a href="mainpage.php?page=feedback">Feedback</a></li>
+                <li><a href="mainpage.php?page=gallery">Gallery</a></li>
+                <li><a href="./">Logout</a></li>
+            </ul>  
+        </nav>
     </div>
-</nav>
+</div>
 
 <body>
-    <div id="content">
+    <div class="container">
+        <div id="content" class="col-xs-12">
             <?php 
                 session_start();
                 if (isset($_GET['page'])) {
+                    if($_GET['page']=="home") include("home.php");
                     if($_GET['page']=="account") include("account.php");
                     if($_GET['page']=="chats") include("chats.php");
                     if($_GET['page']=="feedback") include("feedback.php");
@@ -39,5 +44,6 @@
                 else include("home.php");             
             ?>
         </div>
+    </div>
 </body>
 </html>
