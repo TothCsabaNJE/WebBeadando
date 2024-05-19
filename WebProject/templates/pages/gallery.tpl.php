34233b2edbcb0eada8268fb02e755141f47ce03c
CsomorMate <64906647+matye002@users.noreply.github.com>
2024-05-19 16:04:15 +0200

    Update gallery.tpl.php

--- WebProject/templates/pages/gallery.tpl.php
+++ WebProject/templates/pages/gallery.tpl.php
@@ -1,34 +1,21 @@
-<form action="mainpage.php?oldal=imgUpload" method="post" enctype="multipart/form-data">
-            <label><b>Upload a file:</b></label>
-            <input type="file" id="image" name="image">
-            <input type="submit">
-        </form>
-        <table>
-        <tr>
-        <th>Image Name</th>
-        <th>Image</th>
-        </tr>
-        <?php
-        $files = glob("images/*.*");
-        for ($i = 0; $i < count($files); $i++) {
-            $image = $files[$i];
-            $supported_file = array(
-                'gif',
-                'jpg',
-                'jpeg',
-                'png'
-            );
+    <form action="templates/pages/imgUpload.tpl.php" method="post" enctype="multipart/form-data">
+        <label><b>Upload a file:</b></label>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
 
+    <h2>Gallery</h2>
+    <div class="gallery">
+        <?php
+        $files = glob("../../images/*.*");
+        foreach ($files as $image) {
+            $supported_file = array('gif', 'jpg', 'jpeg', 'png');
             $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
             if (in_array($ext, $supported_file)) {
-            echo "<tr><td>";
-            echo basename($image);
-            echo "</td><td>";
-            echo '<img src="' . $image . '" alt="Random image"/><br>';
-            echo "</td></tr>";
-        } else {
-        continue;
-        }
+                echo "<div class='gallery-item'>";
+                echo "<img src='" . $image . "' alt='" . basename($image) . "'>";
+                echo "</div>";
+            }
         }
         ?>
-        </table>
+    </div>
